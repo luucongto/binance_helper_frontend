daf0f725274b121b1733eb396a30e9cb7dd71ed6
To Luu <to.luu@punch.vn>
2019-06-26 12:57:09 +0700

    Fix host

--- src/Config/ApiConfig.js
+++ src/Config/ApiConfig.js
@@ -1,5 +1,5 @@
 const ApiConfig = {
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/'
 }
 
 export default ApiConfig
